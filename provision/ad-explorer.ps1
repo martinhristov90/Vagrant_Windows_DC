f9c5a0875401767fdc4eed46afe902ecba1d1ba9
Martin Hristov <mhristov@hashicorp.com>
2021-01-18 11:56:36 +0200

    updating the checksum of the ad explorer

--- provision/ad-explorer.ps1
+++ provision/ad-explorer.ps1
@@ -1,7 +1,7 @@
 # install Active Directory Explorer from https://technet.microsoft.com/en-us/sysinternals/adexplorer.aspx
 # NB even though you can use the Windows ADSI Edit application, I find ADExplorer nicer.
 $adExplorerUrl = 'https://download.sysinternals.com/files/AdExplorer.zip'
-$adExplorerHash = '97EF5001C225A869AE739C15AAB1E067B66CE85250FF4D3C265BBFAF09AC8308'
+$adExplorerHash = '3F4137464DF5156C629C8E1827151CBFC7FA99CC10C43E187D7899E6500791EC'
 $adExplorer = 'c:/tmp/AdExplorer.zip' 
 (New-Object System.Net.WebClient).DownloadFile($adExplorerUrl, $adExplorer)
 $adExplorerActualHash = (Get-FileHash $adExplorer -Algorithm SHA256).Hash
